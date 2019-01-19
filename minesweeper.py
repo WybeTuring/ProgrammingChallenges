cfcd06972ea32247ee0a96fbe2c2724560ef9e71
WybeTuring <Karllemfon@gmail.com>
2019-01-19 22:03:30 +0000

    Solving the minecraft problem The problem here was coming up with a function that can read the number of asterics that are in adjacent squares. After that i applied it itteratively to the elements of the imput and printed out the result. I love this, so beautiful.

--- minesweeper.py
+++ minesweeper.py
@@ -2,53 +2,73 @@
 # Date: 13/01/2019
 # Intent: This is an implementation of my proposed solution to the minesweeper problem in the Programming Challenges book
 
-# Function that checks the number of mines arround a particluar position
-def mineCountSingle(arr,i,j,n,m): # Passing the array, the position of the current element and the dimensions of the array into the funtion
-    count = 0 # initializing the mineCountSingle
-    if (i != n-1) and (i != 0) and (j != m-1) and (j != 0): # ensuring the element is not on the border
-        for v in range(i-1,i+2):
-            for h in range(j-1,j+2):
-                if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                    count += 1
-    else:
-        # Taking the cases when the element under consideration is on the edge i.e i = 0, and j=0 or j = m-1 or j=m-1 and i =0 or i = n-1
-        if (i == 0) and (j == 0):
-            for v in range(i,i+2):
-                for h in range(j,j+2):
-                    if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                        count += 1
-        elif (i == 0) and (j == m-1):
-            for v in range(i,i+2):
-                for h in range(j,j-2,-1):
-                    if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                        count += 1
-        elif (i == n-1) and (j == 0):
-            for v in range(i,i-2,-1):
-                for h in range(j,j+2):
-                    if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                        count += 1
-        elif (i == n-1) and (j == m-1):
-            for v in range(i,i-2,-1):
-                for h in range(j,j-2,-1):
-                    if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                        count += 1
-        elif i == 0:
-            for v in range(i-1,i+2):
-                for h in range(j,j+2):
-                    if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                        count += 1
-        elif j == 0:
-            for v in range(i-1,i+2):
-                for h in range(j,j-2,-1):
-                    if (arr[v][h] == '*') and ((v != i) and (h != j)):
-                        count += 1
-        # The function returns the number of mines adjeacent to a position
-        return count
-
-arr1 = [ ['*', '.', '.', '.'],
+import numpy
+arr1 = numpy.array( [['*', '.', '.', '.'],
         ['.', '.', '.', '.'],
         ['.', '*', '.', '.'],
-        ['.', '.', '.', '.']
-]
+        ['.', '.', '.', '.']]
+)
 
-print(mineCountSingle(arr1,2,3,4,4))
+arr2 = numpy.array([
+['*','*','.','.','.'],
+['.','.','.','.','.'],
+['.','*','.','.','.']
+])
+print("This is the first dude")
+# Function that checks the number of mines arround a particluar position
+def mineCount(arr,i,j,m,n):  # The arguments of the funtion are the matrix, i and j for the current position, and m and n for the dimensions
+    count = 0
+    if j-1 >=0: # Ensuring that j-1 does not exceed the size of the mining board
+        if arr[i][j-1] == '*':
+            count += 1
+        if i-1 >=0:
+            if arr[i-1][j-1] == '*':
+                count += 1
+        if i+1<=m-1:
+            if arr[i+1][j-1] == '*':
+                count += 1
+    if j+1<=n-1:
+        if arr[i][j+1] == '*':
+            count += 1
+        if i-1>= 0:
+            if arr[i-1][j+1] == '*':
+                count += 1
+        if i+1 <= m-1:
+            if arr[i+1][j+1] == '*':
+                count += 1
+    if i+1 <= m-1:
+        if arr[i+1][j] == '*':
+            count += 1
+    if i-1 >= 0:
+        if arr[i-1][j] == '*':
+            count += 1
+    return count
+# Trying for the first test
+dimensions = arr1.shape
+answer = []
+for r in range(dimensions[0]):
+    temp = []
+    for s in range(dimensions[1]):
+        if arr1[r][s] == '*':
+            temp.append('*')
+        else:
+            temp.append(mineCount(arr1,r,s,dimensions[0],dimensions[1]))
+    answer.append(temp)
+# Printing the answer in an appropriate manner
+for i in range(len(answer)):
+    print(answer[i])
+
+# Tring for the second test
+dimensions2 = arr2.shape
+answer = []
+for r in range(dimensions2[0]):
+    temp = []
+    for s in range(dimensions2[1]):
+        if arr2[r][s] == '*':
+            temp.append('*')
+        else:
+            temp.append(mineCount(arr2,r,s,dimensions2[0],dimensions2[1]))
+    answer.append(temp)
+# Printing the answer in an appropriate manner
+for i in range(len(answer)):
+    print(answer[i])
